8650972599f33605003843fa7ad0b6445596e75c
maliku@posteo.net <maliku@posteo.net>
2020-10-18 12:46:03 +0200

    .

--- code/espurna/config/version.h
+++ code/espurna/config/version.h
@@ -4,13 +4,9 @@
 
 #pragma once
 
-#define BUILD_MAIN              "1.15.0"
-
 #define APP_NAME                "ESPURNA"
-#ifdef BUILD_ID
-#define APP_VERSION             BUILD_MAIN ## "-dev" ## BUILD_ID
-#else
-#define APP_VERSION             BUILD_MAIN
+#ifndef APP_VERSION
+#define APP_VERSION             "<none>"
 #endif
 #define APP_AUTHOR              "xose.perez@gmail.com"
 #define APP_WEBSITE             "http://tinkerman.cat"
--- code/scripts/copyreleases.py
+++ code/scripts/copyreleases.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+#https://docs.platformio.org/en/latest/projectconf/advanced_scripting.html
+
 Import("env", "projenv")
 
 from shutil import copyfile
@@ -12,7 +14,20 @@ releasesdir = os.path.join(env.subst("$PROJECT_DIR"), "releases")
 os.makedirs(releasesdir, exist_ok=True)
 
 origfile = os.path.join(env.subst("$BUILD_DIR"),env.subst("$PROGNAME")+".bin")
-destfile = os.path.join(releasesdir,env.subst("$PIOENV")+".bin")
+envflags = env.ParseFlags(env['BUILD_FLAGS'])
+cppdefines = envflags.get("CPPDEFINES")
+defines = {}
+for entry in cppdefines:
+    if isinstance(entry, str):
+        defines[entry] = ""
+    elif isinstance(entry, list):
+        defines[entry[0]] = entry[1]
+#print(defines)
+version = defines.get("APP_VERSION")
+if version[0]=="\"" and version [len(version)-1]=="\"":
+    version = version[1:-1]
+#print(version)
+destfile = os.path.join(releasesdir,env.subst("$PIOENV")+"+"+version+".bin")
 
 print("copy file from",origfile,"into",destfile)
 
