ac7ce6ee0770379299e3c0a32127b52140d52897
maliku@posteo.net <maliku@posteo.net>
2020-10-18 04:30:44 +0200

    .

--- code/espurna/config/version.h
+++ code/espurna/config/version.h
@@ -4,8 +4,14 @@
 
 #pragma once
 
+#define BUILD_MAIN              "1.15.0"
+
 #define APP_NAME                "ESPURNA"
-#define APP_VERSION             "1.14.5"
+#ifdef BUILD_ID
+#define APP_VERSION             BUILD_MAIN ## "-dev" ## BUILD_ID
+#else
+#define APP_VERSION             BUILD_MAIN
+#endif
 #define APP_AUTHOR              "xose.perez@gmail.com"
 #define APP_WEBSITE             "http://tinkerman.cat"
 #define CFG_VERSION             5
